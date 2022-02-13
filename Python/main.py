3881a63ea4fd9725bd440cf1e1702d0db3b2545b
Bashir Yesufu <yesufubashir@gmail.com>
2022-02-13 20:27:10 +0100

    Use one line print statement

--- Python/main.py
+++ Python/main.py
@@ -1,4 +1,2 @@
 #Write your code below this line 👇
-print("Day 1 - Python Print function")
-print("The function is declared like this:")
-print("print('what to print')")
+print("Day 1 - Python Print function\nThe function is declared like this:\nprint('what to print')")
