65c74ab1a2077df0aaf6ecc256066dc50e585f48
Bashir Yesufu <yesufubashir@gmail.com>
2022-02-14 19:27:59 +0100

    improve readability

--- Python/life calculator.py
+++ Python/life calculator.py
@@ -1,5 +1,5 @@
 # 🚨 Don't change the code below 👇
-age = input("What is your current age?")
+age = input("What is your current age?\n")
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
@@ -9,4 +9,5 @@ days_left = years_left * 365
 weeks_left = years_left * 52
 months_left = years_left * 12
 
-print(f"You have {days_left} days, {weeks_left} weeks and {months_left} months left")
+message = f"You have {days_left} days, {weeks_left} weeks and {months_left} months left"
+print(message)
