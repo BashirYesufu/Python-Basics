0548a0f42cce4b3db0e70482cb92c84136fc69fa
Bashir Yesufu <yesufubashir@gmail.com>
2022-02-14 14:27:57 +0100

    Add string manipulations

--- Python/Strings.py
+++ Python/Strings.py
@@ -1,2 +1,7 @@
-# Print the length of a string input by the User 👇
-print(len(input("What is your name? ")))
+# This is a comment
+print("Day 1 - String Manipulation")
+print("String Concatenation is done with the '+' sign.")
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+
+print("Hello " + input("What is your name"))
