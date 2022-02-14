40d06faf52c9710a34e7bf9abf6759aae3cd89b8
Bashir Yesufu <yesufubashir@gmail.com>
2022-02-14 22:27:43 +0100

    Add bmi description

--- Python/bmi generator.py
+++ Python/bmi generator.py
@@ -1,10 +1,25 @@
 # 🚨 Don't change the code below 👇
-height = input("enter your height in m: ")
-weight = input("enter your weight in kg: ")
+height = float(input("enter your height in m: "))
+weight = float(input("enter your weight in kg: "))
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
 
-bmi = float(weight) / (float(height) ** 2)
-print(int(bmi))
+bmi = round(weight / (height ** 2))
+print(bmi)
+if bmi < 18.5:
+  print(f"Your bmi is {bmi}, you are underweight.")
+elif bmi < 25:
+  print(f"Your bmi is {bmi}, you have normal weight.")
+elif bmi < 30:
+  print(f"Your bmi is {bmi}, you are overweight.")
+elif bmi < 35:
+  print(f"Your bmi is {bmi}, you are obese.")
+else:
+  print(f"Your bmi is {bmi}, you are clinically obese.")
+
+
+
+
+
 
