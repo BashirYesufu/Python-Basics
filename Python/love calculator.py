4babfd0f3b91a2a475de130630879aa3bffc89e7
Bashir Yesufu <yesufubashir@gmail.com>
2022-02-15 16:15:22 +0100

    convert string love to int

--- Python/love calculator.py
+++ Python/love calculator.py
@@ -8,8 +8,7 @@ name2 = input("What is their name? \n")
 name = name1 + name2
 lower_name_true = name.lower().count("t") + name.lower().count("r") + name.lower().count("u") + name.lower().count("e")
 lower_name_love = name.lower().count("l") + name.lower().count("o") + name.lower().count("v") + name.lower().count("e")
-combined_string = str(lower_name_true) + str(lower_name_love)
-love_score = int(combined_string)
+love_score = int(str(lower_name_true) + str(lower_name_love))
 
 if love_score < 10 or love_score > 90:
   print(f"Your score is {love_score}, you go together like coke and menthos")
@@ -17,4 +16,5 @@ elif love_score >= 40 or love_score <= 50:
   print(f"Your score is {love_score}, you are alright together")
 else:
   print(f"Your score is {love_score}")
+
 
