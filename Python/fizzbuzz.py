24fd8e2f70c950f47ba622e7bc4d9c42c9831f1b
Bashir Yesufu <yesufubashir@gmail.com>
2022-02-17 12:22:34 +0100

    Switch up the elif statements

--- Python/fizzbuzz.py
+++ Python/fizzbuzz.py
@@ -2,9 +2,10 @@
 for number in range(1, 100):
   if number % 3 == 0 and number % 5 == 0:
     print("fizzbuzz")
+  elif number % 3 == 0:
+    print("fizz")
+  elif number % 5 == 0:
+    print("buzz")
   else:
     print(number)
-  if number % 3 == 0:
-    print("fizz")
-  if number % 5 == 0:
-    print("buzz")
+  
