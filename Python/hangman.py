04bf52ce588d506e6fa4a3e507f9071a987b5c8a
Bashir Yesufu <yesufubashir@gmail.com>
2022-02-17 15:20:01 +0100

    add letter checks

--- Python/hangman.py
+++ Python/hangman.py
@@ -1,14 +1,18 @@
 #Step 1
 import random
 word_list = ["aardvark", "baboon", "camel"]
-
-#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
 chosen_word = random.choice(word_list)
-#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-guess = input("Guess a letter: ").lower()
-#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+print(chosen_word)
+
+display = []
+word_length = len(chosen_word)
 for letter in chosen_word:
-  if letter == guess:
-    print("right")
-  else:
-    print("wrong")
+  display += "_"
+print(display)
+  
+guess = input("Guess a letter: ").lower()
+
+for position in range(word_length):
+  if chosen_word[position] == guess:
+    display[position] = chosen_word[position]
+print(display)
