dfbb72a314f899ebad9c9ebd8b126d218b47fb26
Bashir Yesufu <yesufubashir@gmail.com>
2022-02-22 14:42:06 +0100

    complete caesar cipher

--- Python/ceasar cipher.py
+++ Python/ceasar cipher.py
@@ -1,47 +1,31 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',]
-
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 def caesar(start_text, shift_amount, cipher_direction):
   end_text = ""
   if cipher_direction == "decode":
-      shift_amount *= -1
-  for letter in start_text:
-    position = alphabet.index(letter)
-    new_position = position + shift_amount
-    new_letter = alphabet[new_position]
-    end_text += new_letter
-  print(f"The {cipher_direction}d text is {end_text}")
+    shift_amount *= -1
+  for char in start_text:
+    if char in alphabet:
+      position = alphabet.index(char)
+      new_position = position + shift_amount
+      end_text += alphabet[new_position]
+    else:
+      end_text += char
+    
+  print(f"Here's the {cipher_direction}d result: {end_text}")
 
-caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+from art import logo
+print(logo)
 
-# #TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
-# def encrypt(plain_text, shift_amount):
-#   cipher_text = ""
-#   for letter in plain_text:
-#     position = alphabet.index(letter)
-#     new_position = position + shift_amount
-#     new_letter = alphabet[new_position]
-#     cipher_text += new_letter
-#   print(f"The encoded text is {cipher_text}")
-
-
-
-# #Decrypt
-# def decrypt(encoded_text, shift_number):
-#   cipher_text = ""
-#   for letter in encoded_text:
-#     position = alphabet.index(letter)
-#     new_position = position - shift_number
-#     new_letter = alphabet[new_position]
-#     cipher_text += new_letter
-#   print(f"The decoded text is {cipher_text}")
-
-# if direction == "encode":
-#   encrypt(plain_text=text, shift_amount=shift)
-# elif direction == "decode":
-#   decrypt(encoded_text=text, shift_number=shift)
-# else:
-#   print("Invalid command")
+should_continue = True
+while should_continue:
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+  shift = shift % 26
+  
+  caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+  result = input("Type 'yes' if you want to go again. Otherwise type 'no'\n")
+  if result == "no":
+    should_continue = False
+    print("Goodbye!")
