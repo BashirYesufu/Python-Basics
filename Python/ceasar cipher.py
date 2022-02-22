3f8f2dcf4134f15ec9985f432f2834960825573c
Bashir Yesufu <yesufubashir@gmail.com>
2022-02-22 14:08:59 +0100

    combine encryption and decryption

--- Python/ceasar cipher.py
+++ Python/ceasar cipher.py
@@ -4,31 +4,44 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-#TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
-def encrypt(plain_text, shift_amount):
-  cipher_text = ""
-  for letter in plain_text:
+def caesar(start_text, shift_amount, cipher_direction):
+  end_text = ""
+  if cipher_direction == "decode":
+      shift_amount *= -1
+  for letter in start_text:
     position = alphabet.index(letter)
     new_position = position + shift_amount
     new_letter = alphabet[new_position]
-    cipher_text += new_letter
-  print(f"The encoded text is {cipher_text}")
+    end_text += new_letter
+  print(f"The {cipher_direction}d text is {end_text}")
+
+caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+
+# #TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
+# def encrypt(plain_text, shift_amount):
+#   cipher_text = ""
+#   for letter in plain_text:
+#     position = alphabet.index(letter)
+#     new_position = position + shift_amount
+#     new_letter = alphabet[new_position]
+#     cipher_text += new_letter
+#   print(f"The encoded text is {cipher_text}")
 
 
 
-#Decrypt
-def decrypt(encoded_text, shift_number):
-  cipher_text = ""
-  for letter in encoded_text:
-    position = alphabet.index(letter)
-    new_position = position - shift_number
-    new_letter = alphabet[new_position]
-    cipher_text += new_letter
-  print(f"The decoded text is {cipher_text}")
+# #Decrypt
+# def decrypt(encoded_text, shift_number):
+#   cipher_text = ""
+#   for letter in encoded_text:
+#     position = alphabet.index(letter)
+#     new_position = position - shift_number
+#     new_letter = alphabet[new_position]
+#     cipher_text += new_letter
+#   print(f"The decoded text is {cipher_text}")
 
-if direction == "encode":
-  encrypt(plain_text=text, shift_amount=shift)
-elif direction == "decode":
-  decrypt(encoded_text=text, shift_number=shift)
-else:
-  print("Invalid command")
+# if direction == "encode":
+#   encrypt(plain_text=text, shift_amount=shift)
+# elif direction == "decode":
+#   decrypt(encoded_text=text, shift_number=shift)
+# else:
+#   print("Invalid command")
