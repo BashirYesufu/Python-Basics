f8c95394214b193f8e2703c145687f4c6e031497
Bashir Yesufu <yesufubashir@gmail.com>
2022-02-25 13:16:08 +0100

    Display computer first card

--- Python/blackjack.py
+++ Python/blackjack.py
@@ -55,14 +55,19 @@ def calculate_score(card):
 #Hint 5: Deal the user and computer 2 cards each using deal_card() and append().
 user_cards = []
 computer_cards = []
+game_over = False
 
 for _ in range(2):
   user_cards.append(deal_card())
   computer_cards.append(deal_card())
 
-#Hint 6: Create a function called calculate_score() that takes a List of cards as input
-#and returns the score.
-#Look up the sum() function to help you do this.
+user_score = calculate_score(user_cards)
+computer_score = calculate_score(computer_cards)
+print(f"  Your cards: {user_cards}, current score: {user_score}")
+print(f"  Computer's first card: {computer_cards[0]}")
+
+if user_score == 0 or computer_score == 0 or user_score > 21:
+  game_over = True
 
 #Hint 9: Call calculate_score(). If the computer or the user has a blackjack (0) or if the user's score is over 21, then the game ends.
 
