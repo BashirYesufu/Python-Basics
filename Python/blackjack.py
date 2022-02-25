0d4798db16a25a8f429cc0598e246aafb60e72e6
Bashir Yesufu <yesufubashir@gmail.com>
2022-02-25 14:13:39 +0100

    play game using recursion for as long as user wants to

--- Python/blackjack.py
+++ Python/blackjack.py
@@ -34,6 +34,7 @@
 #Hint 4: Create a deal_card() function that uses the List below to *return* a random card.
 #11 is the Ace.
 import random
+from replit import clear
 
 # Add to the computer or user cards
 def deal_card():
@@ -67,42 +68,37 @@ def compare(user_score, computer_score):
   else:
     return "You lost 😤"
     
-
-user_cards = []
-computer_cards = []
-game_over = False
-
-for _ in range(2):
-  user_cards.append(deal_card())
-  computer_cards.append(deal_card())
-
-while not game_over:
-  user_score = calculate_score(user_cards)
-  computer_score = calculate_score(computer_cards)
-  print(f"  Your cards: {user_cards}, current score: {user_score}")
-  print(f"  Computer's first card: {computer_cards[0]}")
+def play_game():
+  user_cards = []
+  computer_cards = []
+  game_over = False
   
-  if user_score == 0 or computer_score == 0 or user_score > 21:
-    game_over = True
-  else:
-    user_should_deal = input("Type 'y' to get another card, type 'n' to pass: ").lower()
-    if user_should_deal == 'y':
-      user_cards.append(deal_card())
+  for _ in range(2):
+    user_cards.append(deal_card())
+    computer_cards.append(deal_card())
+  
+  while not game_over:
+    user_score = calculate_score(user_cards)
+    computer_score = calculate_score(computer_cards)
+    print(f"  Your cards: {user_cards}, current score: {user_score}")
+    print(f"  Computer's first card: {computer_cards[0]}")
+    
+    if user_score == 0 or computer_score == 0 or user_score > 21:
+      game_over = True
+    else:
+      user_should_deal = input("Type 'y' to get another card, type 'n' to pass: ").lower()
+      if user_should_deal == 'y':
+        user_cards.append(deal_card())
+  
+  while computer_score != 0 and computer_score < 17:
+    computer_cards.append(deal_card())
+    computer_score = calculate_score(computer_cards)
+  
+  print(f"  Your final hand: {user_cards}, final_score: {user_score}")
+  print(f"  Computer's final hand: {computer_cards}, final_score: {computer_score}")
+  print(compare(user_score, computer_score))
 
-while computer_score != 0 and computer_score < 17:
-  computer_cards.append(deal_card())
-  computer_score = calculate_score(computer_cards)
 
-print(f"  Your final hand: {user_cards}, final_score: {user_score}")
-print(f"  Computer's final hand: {computer_cards}, final_score: {computer_score}")
-print(compare(user_score, computer_score))
-#Hint 10: If the game has not ended, ask the user if they want to draw another card. If yes, then use the deal_card() function to add another card to the user_cards List. If no, then the game has ended.
-
-#Hint 11: The score will need to be rechecked with every new card drawn and the checks in Hint 9 need to be repeated until the game ends.
-
-#Hint 12: Once the user is done, it's time to let the computer play. The computer should keep drawing cards as long as it has a score less than 17.
-
-#Hint 13: Create a function called compare() and pass in the user_score and computer_score. If the computer and user both have the same score, then it's a draw. If the computer has a blackjack (0), then the user loses. If the user has a blackjack (0), then the user wins. If the user_score is over 21, then the user loses. If the computer_score is over 21, then the computer loses. If none of the above, then the player with the highest score wins.
-
-#Hint 14: Ask the user if they want to restart the game. If they answer yes, clear the console and start a new game of blackjack and show the logo from art.py.
-
+while input("Do you want to play a new game? Type 'y' or 'n': ").lower() == "y":
+  clear()
+  play_game()
