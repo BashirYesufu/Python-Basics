d48436b3a730afaf0dbdef4244b14a9ff4ac507b
Bashir Yesufu <yesufubashir@gmail.com>
2022-03-03 10:29:41 +0100

    add game logic

--- Python/who is popular/main.py
+++ Python/who is popular/main.py
@@ -1,1 +1,15 @@
+# import files
+from random import randint
+from art import logo, vs
+from game_data import data
 
+# Generate random item from the game data
+first = data[randint(0, len(data) - 1)]
+second = data[randint(0, len(data) - 1)]
+
+# Game logic
+print(logo)
+print(first)
+print(second)
+print(len(data))
+print(vs)
