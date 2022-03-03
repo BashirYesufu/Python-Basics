1d7530064fa1d7bc29b6c1bd38f2d4aefb406d1a
Bashir Yesufu <yesufubashir@gmail.com>
2022-03-03 11:59:22 +0100

    Compare user input with actual highest insta popularity

--- Python/who is popular/main.py
+++ Python/who is popular/main.py
@@ -9,7 +9,38 @@ second = data[randint(0, len(data) - 1)]
 
 # Game logic
 print(logo)
-print(first)
-print(second)
-print(len(data))
+print(f"Compare A: {first['name']}, a popular {first['description']} from {first['country']}")
 print(vs)
+print(f"Against B: {second['name']}, a popular {second['description']} from {second['country']}")
+
+print(first)
+
+
+print(second)
+
+user_answer = input(f"Who has more followers on instagram? Type 'A' for {first['name']} or 'B' for {second['name']}:\n").lower()
+
+def check_popularity():
+  if first['follower_count'] > second['follower_count']:
+    return first['follower_count']
+  elif first['follower_count'] < second['follower_count']:
+    return second['follower_count']
+  else:
+    return first['follower_count']
+
+def compare_answer(input):
+  highest_popularity = check_popularity()
+  if input == 'a':
+    if first['follower_count'] == highest_popularity:
+      print('Got it')
+    else:
+      print('Did not get it for a')
+  elif input == 'b':
+    if second['follower_count'] == highest_popularity:
+      print('Got it')
+    else:
+      print('Did not get it from b')
+  else:
+    print('You did not enter a valid option')
+
+compare_answer(user_answer)
