6903c49ee4a10d71923b41ae1cd83bd3f80b43dd
Bashir Yesufu <yesufubashir@gmail.com>
2022-03-06 10:57:00 +0100

    check resources if enough

--- CoffeeMachine/main.py
+++ CoffeeMachine/main.py
@@ -4,6 +4,14 @@ from resources import resources
 profit_made = 0
 continue_dispensing = True
 
+def check_resources(drink_ingredients):
+    for item in drink_ingredients:
+        if drink_ingredients[item] >= resources[item]:
+            print(f"Sorry, there is not enough {item}")
+            return False
+    return True
+
+
 while continue_dispensing:
     choice = input("What would you like? (espresso, latte or cappuccino): ")
     if choice == "off":
@@ -13,3 +21,7 @@ while continue_dispensing:
         print(f"{resources['milk']}ml of milk")
         print(f"{resources['coffee']}g of coffee")
         print(f"${profit_made} profit made.")
+    else:
+        coffee_type = MENU(choice)
+        if check_resources(coffee_type):
+
