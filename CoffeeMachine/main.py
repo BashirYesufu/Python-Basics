86f4cba9d05c5458bee68cd3bb25b111a7d03620
Bashir Yesufu <yesufubashir@gmail.com>
2022-03-06 10:29:20 +0100

    Switch off machine with off command

--- CoffeeMachine/main.py
+++ CoffeeMachine/main.py
@@ -1,1 +1,8 @@
+from menu import MENU
+from resources import  resources
 
+continue_dispensing = True
+while continue_dispensing:
+    choice = input("What would you like? (espresso, latte or cappuccino): ")
+    if choice == "off":
+        continue_dispensing = False
