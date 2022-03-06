5dd5f2ae0f84b726328281e6f1d6c3922ef3ad35
Bashir Yesufu <yesufubashir@gmail.com>
2022-03-06 10:43:24 +0100

    print report

--- CoffeeMachine/main.py
+++ CoffeeMachine/main.py
@@ -1,8 +1,15 @@
 from menu import MENU
-from resources import  resources
+from resources import resources
 
+profit_made = 0
 continue_dispensing = True
+
 while continue_dispensing:
     choice = input("What would you like? (espresso, latte or cappuccino): ")
     if choice == "off":
         continue_dispensing = False
+    elif choice == "report":
+        print(f"{resources['water']}ml of water")
+        print(f"{resources['milk']}ml of milk")
+        print(f"{resources['coffee']}g of coffee")
+        print(f"${profit_made} profit made.")
