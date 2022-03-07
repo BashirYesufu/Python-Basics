d1663046f62f01a3d7da2c965e250ec4dc0ce16d
Bashir Yesufu <yesufubashir@gmail.com>
2022-03-07 21:00:57 +0100

    Add coffee maker

--- oop/main.py
+++ oop/main.py
@@ -1,13 +1,20 @@
 from turtle import Turtle, Screen
+from prettytable import PrettyTable
 
-timmy = Turtle()
-print(timmy)
-timmy.shape("turtle")
-timmy.color("coral")
-timmy.forward(200)
-timmy.left(300)
-timmy.left(500)
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("coral")
+# timmy.forward(200)
+# timmy.left(300)
+# timmy.left(500)
 
-my_screen = Screen()
-print(my_screen.canvheight)
-my_screen.exitonclick()
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+
+table = PrettyTable()
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+table.align = "l"
+print(table)
