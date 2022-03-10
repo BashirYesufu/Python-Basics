dd1fff1ad915cddd47342934484c5b18215c9faf
Bashir Yesufu <yesufubashir@gmail.com>
2022-03-10 21:09:00 +0100

    Use pen up and pen down

--- HirstPainting/main.py
+++ HirstPainting/main.py
@@ -8,9 +8,9 @@ timmy.shape('turtle')
 timmy.color("blue")
 for _ in range(10):
     timmy.forward(10)
-    timmy.color("white")
+    timmy.penup()
     timmy.forward(10)
-    timmy.color("blue")
+    timmy.pendown()
 
 
 screen = Screen()
