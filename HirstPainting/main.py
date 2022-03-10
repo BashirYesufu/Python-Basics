c5cfc451a8de58b7ee16ac9c8fa28ed9807c8bad
Bashir Yesufu <yesufubashir@gmail.com>
2022-03-10 20:51:31 +0100

    rename turtle to timmy

--- HirstPainting/main.py
+++ HirstPainting/main.py
@@ -1,16 +1,11 @@
 from turtle import Turtle, Screen
 
-timmy_the_turtle = Turtle()
-timmy_the_turtle.shape('turtle')
-timmy_the_turtle.color("blue")
-timmy_the_turtle.forward(100)
-timmy_the_turtle.right(90)
-timmy_the_turtle.forward(100)
-timmy_the_turtle.right(90)
-timmy_the_turtle.forward(100)
-timmy_the_turtle.right(90)
-timmy_the_turtle.forward(100)
-timmy_the_turtle.right(90)
+timmy = Turtle()
+timmy.shape('turtle')
+timmy.color("blue")
+for _ in range(4):
+    timmy.forward(100)
+    timmy.right(90)
 
 screen = Screen()
 screen.exitonclick()
