d09caef9b6482addc8df0ede6e68af77a68bf759
Bashir Yesufu <yesufubashir@gmail.com>
2022-03-12 00:55:41 +0100

    Extract color from painting using colorgram

--- HirstPainting/main.py
+++ HirstPainting/main.py
@@ -1,33 +1,22 @@
-import turtle as t
-import random
+import colorgram
 
-t.colormode(255)
+rgb_colors = []
+colors = colorgram.extract('image.jpg', 60)
 
-timmy = t.Turtle()
-
-
-# Generate random colors using R G B
-def get_random_color():
-    r = random.randint(0, 255)
-    g = random.randint(0, 255)
-    b = random.randint(0, 255)
-    color = (r, g, b)
-    return color
+for color in colors:
+    r = color.rgb.r
+    g = color.rgb.g
+    b = color.rgb.b
+    new_color = (r, g, b)
+    rgb_colors.append(new_color)
 
 
-timmy.speed(20)
-
-
-def draw_circle(gap_size):
-    for _ in range(int(350 / gap_size)):
-        timmy.color(get_random_color())
-        timmy.circle(100)
-        current_heading = timmy.heading()
-        timmy.setheading(current_heading + gap_size)
-
-
-draw_circle(5)
-screen = t.Screen()
-screen.exitonclick()
+print(rgb_colors)
+color_list = [(202, 164, 110), (240, 245, 241), (236, 239, 243), (149, 75, 50), (222, 201, 136), (53, 93, 123),
+              (170, 154, 41), (138, 31, 20), (134, 163, 184), (197, 92, 73), (47, 121, 86), (73, 43, 35),
+              (145, 178, 149), (14, 98, 70), (232, 176, 165), (160, 142, 158), (54, 45, 50), (101, 75, 77),
+              (183, 205, 171), (36, 60, 74), (19, 86, 89), (82, 148, 129), (147, 17, 19), (27, 68, 102), (12, 70, 64),
+              (107, 127, 153), (176, 192, 208), (168, 99, 102), (66, 64, 60), (219, 178, 183), (178, 198, 202),
+              (112, 139, 141), (254, 194, 0)]
 
 
