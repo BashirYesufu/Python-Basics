9841472a68c07dfe7d89bf311f5f2c18defaa511
Bashir Yesufu <yesufubashir@gmail.com>
2022-03-12 21:16:09 +0100

    change turtle color mode

--- HirstPainting/main.py
+++ HirstPainting/main.py
@@ -1,17 +1,10 @@
-import colorgram
+import random
+import turtle as t
 
-rgb_colors = []
-colors = colorgram.extract('image.jpg', 60)
+t.colormode(255)
 
-for color in colors:
-    r = color.rgb.r
-    g = color.rgb.g
-    b = color.rgb.b
-    new_color = (r, g, b)
-    rgb_colors.append(new_color)
+timmy = t.Turtle()
 
-
-print(rgb_colors)
 color_list = [(202, 164, 110), (240, 245, 241), (236, 239, 243), (149, 75, 50), (222, 201, 136), (53, 93, 123),
               (170, 154, 41), (138, 31, 20), (134, 163, 184), (197, 92, 73), (47, 121, 86), (73, 43, 35),
               (145, 178, 149), (14, 98, 70), (232, 176, 165), (160, 142, 158), (54, 45, 50), (101, 75, 77),
@@ -19,4 +12,5 @@ color_list = [(202, 164, 110), (240, 245, 241), (236, 239, 243), (149, 75, 50), 
               (107, 127, 153), (176, 192, 208), (168, 99, 102), (66, 64, 60), (219, 178, 183), (178, 198, 202),
               (112, 139, 141), (254, 194, 0)]
 
+timmy.dot(20, random.choice(color_list))
 
