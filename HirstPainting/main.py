a97d88732607248f70f2962a027adf006c25187c
Bashir Yesufu <yesufubashir@gmail.com>
2022-03-12 21:46:33 +0100

    increase turtle speed and lift pen up

--- HirstPainting/main.py
+++ HirstPainting/main.py
@@ -4,6 +4,7 @@ import turtle as t
 t.colormode(255)
 
 timmy = t.Turtle()
+timmy.speed('fastest')
 
 color_list = [(202, 164, 110), (240, 245, 241), (236, 239, 243), (149, 75, 50), (222, 201, 136), (53, 93, 123),
               (170, 154, 41), (138, 31, 20), (134, 163, 184), (197, 92, 73), (47, 121, 86), (73, 43, 35),
@@ -16,34 +17,25 @@ timmy.setheading(225)
 timmy.penup()
 timmy.forward(350)
 timmy.setheading(0)
-timmy.pendown()
 
 
 def go_forward():
     for _ in range(10):
         timmy.dot(20, random.choice(color_list))
-        timmy.penup()
         timmy.forward(50)
-        timmy.pendown()
         timmy.dot(20, random.choice(color_list))
     timmy.setheading(90)
-    timmy.penup()
     timmy.forward(50)
-    timmy.pendown()
     timmy.setheading(180)
 
 
 def go_backwards():
     for _ in range(10):
         timmy.dot(20, random.choice(color_list))
-        timmy.penup()
         timmy.forward(50)
-        timmy.pendown()
         timmy.dot(20, random.choice(color_list))
     timmy.setheading(90)
-    timmy.penup()
     timmy.forward(50)
-    timmy.pendown()
     timmy.setheading(0)
 
 
