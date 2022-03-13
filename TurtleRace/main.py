d796f3f69e21075b82cf83c00d53823e54328659
Bashir Yesufu <yesufubashir@gmail.com>
2022-03-13 21:29:13 +0100

    use listener to bind function to a key

--- TurtleRace/main.py
+++ TurtleRace/main.py
@@ -1,1 +1,14 @@
+from turtle import Turtle, Screen
 
+timmy = Turtle()
+timmy.shape("turtle")
+
+
+def move_forward():
+    timmy.forward(10)
+
+
+screen = Screen()
+screen.listen()
+screen.onkey(key="space", fun=move_forward)
+screen.exitonclick()
