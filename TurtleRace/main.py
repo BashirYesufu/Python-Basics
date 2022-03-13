fda73bf21799ee808d08acb03ed0ba1ea99a5cef
Bashir Yesufu <yesufubashir@gmail.com>
2022-03-13 21:57:54 +0100

    create colored turtles and assign start position

--- TurtleRace/main.py
+++ TurtleRace/main.py
@@ -1,14 +1,16 @@
 from turtle import Turtle, Screen
 
-timmy = Turtle()
-timmy.shape("turtle")
-
-
-def move_forward():
-    timmy.forward(10)
-
-
 screen = Screen()
-screen.listen()
-screen.onkey(key="space", fun=move_forward)
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="Place your bets", prompt="Which turtle will win the race? Enter a color: ")
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+
+y_position = -100
+for color in colors:
+    timmy = Turtle(shape="turtle")
+    timmy.color(color)
+    timmy.penup()
+    timmy.goto(x=-230, y=y_position)
+    y_position += 50
+
 screen.exitonclick()
