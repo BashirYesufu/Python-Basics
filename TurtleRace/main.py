ee0f3452b08625376df0f9f7d6a932691585c0a2
Bashir Yesufu <yesufubashir@gmail.com>
2022-03-13 22:09:10 +0100

    move all turtles by a random speed

--- TurtleRace/main.py
+++ TurtleRace/main.py
@@ -1,16 +1,29 @@
 from turtle import Turtle, Screen
+import random
 
+keep_racing = False
 screen = Screen()
 screen.setup(width=500, height=400)
 user_bet = screen.textinput(title="Place your bets", prompt="Which turtle will win the race? Enter a color: ")
 colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+turtles = []
 
-y_position = -100
+y_position = -70
 for color in colors:
-    timmy = Turtle(shape="turtle")
-    timmy.color(color)
-    timmy.penup()
-    timmy.goto(x=-230, y=y_position)
-    y_position += 50
+    new_turtle = Turtle(shape="car")
+    new_turtle.color(color)
+    new_turtle.penup()
+    new_turtle.goto(x=-230, y=y_position)
+    y_position += 30
+    turtles.append(new_turtle)
+
+if user_bet:
+    keep_racing = True
+
+while keep_racing:
+    for turtle in turtles:
+        distance = random.randint(0, 10)
+        turtle.forward(distance)
+
 
 screen.exitonclick()
