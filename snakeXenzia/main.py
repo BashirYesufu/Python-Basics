024a3f71ac43dcc506689380f831a67ec3ccb116
Bashir Yesufu <yesufubashir@gmail.com>
2022-03-14 21:38:45 +0100

    Add snake body and movement capabilities

--- snakeXenzia/main.py
+++ snakeXenzia/main.py
@@ -1,1 +1,25 @@
+from turtle import Screen, Turtle
 
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Xenzia")
+
+x_axis = 0
+y_axis = 0
+snake_body = []
+
+for _ in range(3):
+    new_segment = Turtle("square")
+    new_segment.penup()
+    new_segment.color("white")
+    new_segment.goto(x=x_axis, y=y_axis)
+    x_axis -= 20
+    snake_body.append(new_segment)
+
+game_is_on = True
+while game_is_on:
+    for segment in snake_body:
+        segment.forward(20)
+
+screen.exitonclick()
