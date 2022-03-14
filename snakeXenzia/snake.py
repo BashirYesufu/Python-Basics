94fd17cd0d05f9d960ff880485aa37749f716553
Bashir Yesufu <yesufubashir@gmail.com>
2022-03-14 23:38:49 +0100

    creat directional movement methods

--- snakeXenzia/snake.py
+++ snakeXenzia/snake.py
@@ -1,12 +1,16 @@
 from turtle import Turtle
 MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 
 
 class Snake:
     def __init__(self):
         self.snake_body = []
         self.create_snake()
-        self.move()
+        self.head = self.snake_body[0]
 
     def create_snake(self):
         for _ in range(3):
@@ -23,4 +27,20 @@ class Snake:
             new_x = self.snake_body[segment_number - 1].xcor()
             new_y = self.snake_body[segment_number - 1].ycor()
             self.snake_body[segment_number].goto(new_x, new_y)
-        self.snake_body[0].forward(MOVE_DISTANCE)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if not self.head.heading() == UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if not self.head.heading() == RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if not self.head.heading() == LEFT:
+            self.head.setheading(RIGHT)
