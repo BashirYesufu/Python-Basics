64e6ee85cf18664d88c0087075913901f0309010
Bashir Yesufu <yesufubashir@gmail.com>
2022-03-15 21:24:18 +0100

    update score as snake eats food

--- snakeXenzia/scoreboard.py
+++ snakeXenzia/scoreboard.py
@@ -4,4 +4,14 @@ from turtle import Turtle
 class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
+        self.score = 0
+        self.penup()
+        self.goto(0, 270)
+        self.color("white")
+        self.write(f"Score: {self.score}", align="center", font=("Arial", 24, "normal"))
+        self.hideturtle()
 
+    def update_score(self):
+        self.clear()
+        self.score += 1
+        self.write(f"Score: {self.score}", align="center", font=("Arial", 24, "normal"))
