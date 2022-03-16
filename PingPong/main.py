7147f5fb5556f2fc419469092c8b6171f66015af
Bash_Grey <yesufubashir@gmail>
2022-03-16 21:28:55 +0100

    refactor paddle to use OOP

--- PingPong/main.py
+++ PingPong/main.py
@@ -1,4 +1,5 @@
-from turtle import Turtle, Screen
+from turtle import Screen
+from paddle import Paddle
 
 screen = Screen()
 screen.bgcolor("black")
@@ -6,32 +7,17 @@ screen.setup(width=800, height=600)
 screen.title("Ping Pong")
 screen.tracer(0)
 
-paddle = Turtle()
-paddle.shape("square")
-paddle.color("white")
-paddle.shapesize(stretch_wid=5, stretch_len=1)
-paddle.penup()
-paddle.goto(350, 0)
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
 
-
-def go_up():
-    y_position = paddle.ycor() + 20
-    paddle.goto(paddle.xcor(), y_position)
-
-
-def go_down():
-    y_position = paddle.ycor() - 20
-    paddle.goto(paddle.xcor(), y_position)
-
-
-game_is_on = True
-
-
-while game_is_on:
-    screen.update()
 
 screen.listen()
-screen.onkey(go_up, "Up")
-screen.onkey(go_down, "Down")
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
 
+game_is_on = True
+while game_is_on:
+    screen.update()
 screen.exitonclick()
