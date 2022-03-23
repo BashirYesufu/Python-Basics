916bc270d4962152827d55881e70d46069cd0a30
Bash_Grey <yesufubashir@gmail.com>
2022-03-23 23:26:15 +0100

    Add input and entry widgets

--- Unit Converter/main.py
+++ Unit Converter/main.py
@@ -1,11 +1,29 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("Unit Converter")
 window.minsize(width=500, height=300)
 
 # Label
-my_label = tkinter.Label(text="New Label", font=("Arial", 24, "bold"))
+my_label = Label(text="New Label", font=("Arial", 24, "bold"))
 my_label.pack()
+my_label["text"] = "New Text"
+my_label.config(text="Convert")
+
+
+# Button
+def button_clicked():
+    new_text = input_field.get()
+    my_label.config(text=new_text)
+
+
+button = Button(text="Click Me", command=button_clicked)
+button.pack()
+
+# Entry
+
+input_field = Entry(width=10)
+input_field.pack()
+
 
 window.mainloop()
