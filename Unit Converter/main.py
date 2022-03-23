55e77effe9867517d4480d96ffa6c980eff2d479
Bash_Grey <yesufubashir@gmail.com>
2022-03-24 00:09:58 +0100

    pad tkinter widgets

--- Unit Converter/main.py
+++ Unit Converter/main.py
@@ -3,12 +3,13 @@ from tkinter import *
 window = Tk()
 window.title("Unit Converter")
 window.minsize(width=500, height=300)
+window.config(padx=20, pady=20)
 
 # Label
 my_label = Label(text="New Label", font=("Arial", 24, "bold"))
-my_label.pack()
 my_label["text"] = "New Text"
 my_label.config(text="Convert")
+my_label.grid(column=0, row=0)
 
 
 # Button
@@ -18,12 +19,12 @@ def button_clicked():
 
 
 button = Button(text="Click Me", command=button_clicked)
-button.pack()
+button.grid(column=2, row=0)
 
 # Entry
 
 input_field = Entry(width=10)
-input_field.pack()
+input_field.grid(column=2, row=2)
 
 
 window.mainloop()
