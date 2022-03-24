3b1dacb069259a04426553a7386f959c56ba5889
Bash_Grey <yesufubashir@gmail.com>
2022-03-24 21:51:15 +0100

    convert miles to kilometre

--- Unit Converter/main.py
+++ Unit Converter/main.py
@@ -1,30 +1,32 @@
 from tkinter import *
 
+
+def miles_to_km():
+    miles = float(miles_input.get())
+    km = round(miles * 1.609)
+    kilometer_result_label.config(text=f"{km}")
+
+
 window = Tk()
 window.title("Unit Converter")
-window.minsize(width=500, height=300)
 window.config(padx=20, pady=20)
 
-# Label
-my_label = Label(text="New Label", font=("Arial", 24, "bold"))
-my_label["text"] = "New Text"
-my_label.config(text="Convert")
-my_label.grid(column=0, row=0)
+miles_input = Entry(width=10)
+miles_input.grid(column=1, row=0)
 
+miles_label = Label(text="Miles")
+miles_label.grid(column=2, row=0)
 
-# Button
-def button_clicked():
-    new_text = input_field.get()
-    my_label.config(text=new_text)
+is_equal_label = Label(text="is equal to")
+is_equal_label.grid(column=0, row=1)
 
+kilometer_result_label = Label(text="0")
+kilometer_result_label.grid(column=1, row=1)
 
-button = Button(text="Click Me", command=button_clicked)
-button.grid(column=2, row=0)
+kilometer_label = Label(text="Km")
+kilometer_label.grid(column=2, row=1)
 
-# Entry
-
-input_field = Entry(width=10)
-input_field.grid(column=2, row=2)
-
+calculate_button = Button(text="Calculate", command=miles_to_km)
+calculate_button.grid(column=1, row=2)
 
 window.mainloop()
