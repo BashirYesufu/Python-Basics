2416d80dd8d917f34a4427b1acb7b2f07ac59fb8
Bash_Grey <yesufubashir@gmail.com>
2022-03-24 23:02:15 +0100

    setup UI

--- Pomodoro/main.py
+++ Pomodoro/main.py
@@ -18,12 +18,22 @@ LONG_BREAK_MIN = 20
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodoro")
-window.config(padx=200, pady=200)
+window.config(padx=200, pady=200, bg=YELLOW)
 
-canvas = Canvas(width=200, height=224)
+
+timer_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50))
+timer_label.grid(column=1, row=0)
+
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0 )
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
-canvas.pack()
+canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.grid(column=1, row=1)
 
+start_button = Button(text="Start")
+start_button.grid(column=0, row=2)
+
+reset_button = Button(text="Reset")
+reset_button.grid(column=2, row=2)
 
 window.mainloop()
