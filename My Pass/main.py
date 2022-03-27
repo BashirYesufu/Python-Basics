d343a41bd51a7b824feb1c806588b49da5fd8ff5
Bash_Grey <yesufubashir@gmail.com>
2022-03-27 19:51:26 +0100

    Save data in new file

--- My Pass/main.py
+++ My Pass/main.py
@@ -1,7 +1,19 @@
 from tkinter import *
+
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+    website_data = website_entry.get()
+    email_data = email_entry.get()
+    password_data = password_entry.get()
+
+    with open("data.txt", "a") as data_file:
+        data_file.write(f"{website_data} | {email_data} | {password_data}\n")
+        website_entry.delete(0, END)
+        password_entry.delete(0, END)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -23,17 +35,19 @@ password = Label(text="Password:")
 password.grid(row=3, column=0)
 
 # Entries
-website_entry = Entry(width=35)
+website_entry = Entry(width=40)
 website_entry.grid(row=1, column=1, columnspan=2)
-email_entry = Entry(width=35)
+website_entry.focus()
+email_entry = Entry(width=40)
 email_entry.grid(row=2, column=1, columnspan=2)
+email_entry.insert(0, "bash@mail.com")
 password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1)
 
 # Buttons
 generate_password_button = Button(text="Generate Password")
 generate_password_button.grid(row=3, column=2)
-add_button = Button(text="Add", width=36)
+add_button = Button(text="Add", width=36, command=save)
 add_button.grid(row=4, column=1, columnspan=2)
 
 window.mainloop()
