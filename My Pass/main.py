e6cdf112974a53136415c748ff63ea6e2a5d937d
Bash_Grey <yesufubashir@gmail.com>
2022-03-27 20:51:49 +0100

    Add password generation code

--- My Pass/main.py
+++ My Pass/main.py
@@ -1,8 +1,39 @@
 from tkinter import *
 from tkinter import messagebox
-
+import random
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+# Password Generator Project
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+           'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+           'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+nr_letters = random.randint(8, 10)
+nr_symbols = random.randint(2, 4)
+nr_numbers = random.randint(2, 4)
+
+password_list = []
+
+for char in range(nr_letters):
+    password_list.append(random.choice(letters))
+
+for char in range(nr_symbols):
+    password_list += random.choice(symbols)
+
+for char in range(nr_numbers):
+    password_list += random.choice(numbers)
+
+random.shuffle(password_list)
+
+password = ""
+for char in password_list:
+    password += char
+
+print(f"Your password is: {password}")
+
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
@@ -20,6 +51,7 @@ def save():
                 data_file.write(f"{website_data} | {email_data} | {password_data}\n")
                 website_entry.delete(0, END)
                 password_entry.delete(0, END)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
